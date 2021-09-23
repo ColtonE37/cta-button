2030bd15e34bd61b02915c2d1bd18af1d75de25a
ColtonE37 <80788513+ColtonE37@users.noreply.github.com>
2021-09-22 21:17:47 -0400

    Button Changes

--- src/CtaButton.js
+++ src/CtaButton.js
@@ -8,24 +8,24 @@ export class CtaButton extends LitElement {
         padding: 5px;
         color: var(--cta-button-text-color, #000);
         --ctabuttonTextColor: #ffffff;
-        --disabledbuttonBackground: #ffffff
+        --disabledbuttonBackground: grey;
         --ctabuttonFont: 'Times New Roman', sans-serif;
       }
-
-      button {
+      .assignment {
         color: var(--cta-button-text-color);
         font-size: 25px;
         font-family: var(--ctabuttonFont);
         border-radius: 12px;
+        border: 1px solid;
+        text-decoration: none;
+        padding: 6px;
       }
-
-      button:hover,
-      button:focus,
-      button:active {
+      .assignment:hover,
+      .assignment:focus,
+      .assignment:active {
         background-color: red;
       }
-
-      button:disabled {
+      .assignment:disabled {
         color: var(--ctabuttonTextColor);
         background-color: var(--disabledbuttonBackground);
         cursor: not-allowed;
@@ -35,22 +35,26 @@ export class CtaButton extends LitElement {
 
   static get properties() {
     return {
-      title: { type: String},
-      link: { type: String},
-      disabled: { type: Boolean, reflect: true}
+      title: { type: String },
+      link: { type: String },
+      disabled: { type: Boolean, reflect: true }
     };
   }
 
   constructor() {
     super();
     this.link = "https://me.me/i/yes-neiniegerierator-ie-yes-i-did-it-victory-baby-861706f5aa474036a024011734c5a374"
-    this.disabled = false;
+    this.disabled = true;
+  }
+
+  _navigateToLink() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+    window.open(this.link, "_blank");
   }
 
   render() {
     return html`
-  <a href=${this.buttonLink} tabindex="-1" rel="noreferrer" target="_blank" role="button"></a>
-
-    `;
+  <button class="assignment" @click="${this._navigateToLink}" ?disabled="${this.disabled}">Click Here!</button>
+      `;
   }
 }
