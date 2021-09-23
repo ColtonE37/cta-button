2c7a1e3aab08ebc25fe9913b5d23f83ccdd758eb
ColtonE37 <80788513+ColtonE37@users.noreply.github.com>
2021-09-23 13:07:37 -0400

    Button Improvements

--- src/CtaButton.js
+++ src/CtaButton.js
@@ -6,28 +6,32 @@ export class CtaButton extends LitElement {
       :host {
         display: inline-block;
         padding: 5px;
-        color: var(--cta-button-text-color, #000);
-        --ctabuttonTextColor: #ffffff;
-        --disabledbuttonBackground: grey;
-        --ctabuttonFont: 'Times New Roman', sans-serif;
+        --ctabuttonColor: black;
+        --ctabuttonBackgroundColor: white;
+        --ctabuttonActiveBackgroundColor: red;
+        --ctabuttonActiveColor: white;
+        --ctabuttonDisabledBackgroundColor: grey;
+        --ctabuttonFontFamily: 'Times New Roman', sans-serif;
       }
       .assignment {
-        color: var(--cta-button-text-color);
+        background-color: var(--ctabuttonBackgroundColor: white;);
+        color: var(--ctabuttonColor);
         font-size: 25px;
-        font-family: var(--ctabuttonFont);
+        font-family: var(--ctabuttonFontFamily);
         border-radius: 12px;
-        border: 1px solid;
+        border: 2px solid;
         text-decoration: none;
         padding: 6px;
       }
       .assignment:hover,
       .assignment:focus,
       .assignment:active {
-        background-color: red;
+        background-color: var(--ctabuttonActiveBackgroundColor);
+        color: var(--ctabuttonActiveColor);
       }
       .assignment:disabled {
-        color: var(--ctabuttonTextColor);
-        background-color: var(--disabledbuttonBackground);
+        color: var(--ctabuttonColor);
+        background-color: var(--ctabuttonDisabledBackgroundColor);
         cursor: not-allowed;
       }
     `;
@@ -43,18 +47,21 @@ export class CtaButton extends LitElement {
 
   constructor() {
     super();
+    this.title = "Click Here!"
     this.link = "https://me.me/i/yes-neiniegerierator-ie-yes-i-did-it-victory-baby-861706f5aa474036a024011734c5a374"
-    this.disabled = true;
+    this.disabled = false;
   }
 
-  _navigateToLink() {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Window/open
-    window.open(this.link, "_blank");
-  }
+  // _navigateToLink() {
+  //   // https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+  //   window.open(this.link, "_blank");
+  // }
 
   render() {
     return html`
-  <button class="assignment" @click="${this._navigateToLink}" ?disabled="${this.disabled}">Click Here!</button>
+    <a href="${this.link}" target="_blank" tabindex="-1" rel="noopener noreferrer" >
+      <button class="assignment" ?disabled="${this.disabled}"> ${this.title}</button>
+    </a>
       `;
   }
 }
